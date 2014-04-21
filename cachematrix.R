b1c575726aa7851a7b42e7403213ceb77d3a00ba
jalalomary <jalalaldeen@gmail.com>
2014-04-21 16:20:16 +0300

    Comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R file which cache matrix inverse in order to make it faster
+## File has two functions: makeCacheMatrix and cacheSolve
 
-## Write a short comment describing this function
+## The first function, makeCacheMatrix creates a special "matrix"
+## which is really a list containing a function to
+## * set the value of the matrix
+## * get the value of the matrix
+## * set the value of the inverse
+## * get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
@@ -17,7 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
          getinverse = getinverse)
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of the 
+## special "matrix" created with the above function.
+## However, it first checks to see if the inverse has already 
+## been calculated. If so, it gets the inverse from the cache and 
+## skips the computation. Otherwise, it calculates the inverse 
+## of the data and sets the value of the inverse in the cache 
+## via the setinverse function.
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
